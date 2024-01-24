fdb910c4dc5a549309ea0ee37a31e096dea1d9f5
ibnshayed <ibnshayed@gmail.com>
2024-01-24 20:56:14 +0600

    dropdown updated

--- src/components/dorpdowns/dropdown.tsx
+++ src/components/dorpdowns/dropdown.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
-const Dropdown = ({ children }) => {
-	const [open, setOpen] = useState(false)
-	const handleClick  = () => setOpen(!open)
-	return <div>{ children(handleClick, open)}</div>;
+const Dropdown = ({ children }: { children: (handleClick: () => void, open: boolean) => ReactNode }) => {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => setOpen(!open);
+  return <div>{children(handleClick, open)}</div>;
 };
 
 export default Dropdown;
