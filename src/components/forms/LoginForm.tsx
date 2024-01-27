b0596c045ef7115956ecb73f77b28176d92004ba
ibnshayed <ibnshayed@gmail.com>
2024-01-27 23:11:06 +0600

    login form updated

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { SubmitHandler, useForm } from "react-hook-form";
+import TextInput from "./TextInput";
 
 type Inputs = {
   username: string;
@@ -13,7 +14,7 @@ export default function LoginForm() {
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data, errors);
 
   // console.log(watch("username")); // watch input value by passing the name of it
 
@@ -21,36 +22,36 @@ export default function LoginForm() {
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
       {/* register your input into the hook by invoking the "register" function */}
-      <div className="space-y-2">
-        <label className="text-sm">Username</label>
-        <input
-          // defaultValue="test"
-          {...register("username")}
-          placeholder="Enter your username"
-          className="block w-full bg-transparent p-2 border-2 focus:border-blue-500 rounded-lg outline-none"
-        />
-      </div>
+      <TextInput
+        {...register("username", {
+					maxLength: {
+						value: 8,
+						message: "username length should not be greater then 8"
+					},
+        })}
+        label="Username or Email"
+        placeholder="Enter your username"
+        type="text"
+        errors={errors}
+      />
 
       {/* include validation with required or other standard HTML validation rules */}
-      <div className="space-y-2">
-        <label className="text-sm">Password</label>
-        <input
-          {...register("password", { required: true })}
-          placeholder="Enter your password"
-          type="password"
-          className="block w-full p-2 border-2 focus:border-blue-500 rounded-lg outline-none"
-        />
-        {errors.password && (
-          <small className="text-red-500">This field is required</small>
-        )}
-        {/* errors will return when field validation fails  */}
-      </div>
+      <TextInput
+        {...register("password", { required: "This field is required" })}
+        label="Password"
+        type="password"
+        placeholder="Enter your password"
+        errors={errors}
+      />
+      {/* errors will return when field validation fails  */}
 
       <div>
-        <input
+        <button
           type="submit"
-          className="block w-full bg-blue-500 text-center text-white rounded-lg py-2 cursor-pointer"
-        />
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Sign up
+        </button>
       </div>
     </form>
   );
---
+++ src/components/forms/TextInput.tsx
@@ -0,0 +1,34 @@
+import { ErrorMessage } from "@hookform/error-message";
+import { forwardRef } from "react";
+
+const Input = (
+  { label, error, errors, onChange, onBlur, name, ...otherProps }: any,
+  ref: any
+) => {
+  console.log("🚀 ~ error:", error);
+  return (
+    <div className="space-y-1">
+      <label className="block text-sm font-medium text-gray-700">{label}</label>
+      <input
+        name={name}
+        ref={ref}
+        onChange={onChange}
+        onBlur={onBlur}
+        {...otherProps}
+      />
+      <ErrorMessage
+        errors={errors}
+        name={name}
+        render={({ message }) => (
+          <small className="block text-sm font-medium text-red-700">
+            {message}
+          </small>
+        )}
+      />
+    </div>
+  );
+};
+
+const TextInput = forwardRef(Input);
+
+export default TextInput;
